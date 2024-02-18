6ab0be8155d74fd29f8efa0a6de437a725dfeefd
4dot4 <controlgame@icloud.com>
2024-02-17 23:41:46 -0300

    inital commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gameoflife
+
+go 1.22rc2
+
+require (
+	github.com/ebitengine/purego v0.6.0 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20240125111008-83d871a38f28 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type cell struct {
+	Pos   rl.Vector2
+	State bool
+}
+
+func update(grid *[50][50]bool) {
+
+	m := make(map[cell]int)
+	for y := range grid {
+		for x := range grid[y] {
+			friendsCounter := 0
+			if y > 0 {
+				if x > 0 {
+					if grid[y-1][x-1] { //superior esquerda
+						friendsCounter++
+					}
+				}
+				if grid[y-1][x] { //superior central
+					friendsCounter++
+				}
+				if x < len(grid[y])-1 {
+					if grid[y-1][x+1] { //superior direita
+						friendsCounter++
+					}
+
+				}
+			}
+			if x > 0 {
+				if grid[y][x-1] { //meia esquerda
+					friendsCounter++
+				}
+
+			}
+			if x < len(grid[y])-1 {
+				if grid[y][x+1] { //meia direita
+					friendsCounter++
+				}
+
+			}
+			if y < len(grid)-1 {
+				if x > 0 {
+					if grid[y+1][x-1] { //baixo esquerda
+						friendsCounter++
+					}
+
+				}
+				if grid[y+1][x] { //baixo central
+					friendsCounter++
+				}
+				if x < len(grid[y])-1 {
+					if grid[y+1][x+1] { //baixo direita
+						friendsCounter++
+					}
+				}
+			}
+			m[cell{rl.Vector2{X: float32(x), Y: float32(y)}, grid[y][x]}] = friendsCounter
+		}
+	}
+	for k, friends := range m {
+		if k.State {
+			if friends < 2 {
+				grid[int32(k.Pos.Y)][int32(k.Pos.X)] = false
+			} else if friends == 2 || friends == 3 {
+				grid[int32(k.Pos.Y)][int32(k.Pos.X)] = true
+			} else if friends > 3 {
+				grid[int32(k.Pos.Y)][int32(k.Pos.X)] = false
+			}
+
+		} else if !grid[int32(k.Pos.Y)][int32(k.Pos.X)] && friends == 3 {
+			grid[int32(k.Pos.Y)][int32(k.Pos.X)] = true
+		}
+	}
+
+}
+func main() {
+	var grid [50][50]bool
+	rl.InitWindow(800, 800, "Life")
+	rl.SetTargetFPS(60)
+	rl.SetWindowState(rl.FlagWindowResizable)
+	// colors := []rl.Color{rl.Blue, rl.Red, rl.Orange, rl.Green}
+	fpsCounter := 0
+	start := false
+	for !rl.WindowShouldClose() {
+		fpsCounter++
+		if rl.IsKeyPressed(rl.KeyS) {
+			start = true
+		} else if rl.IsKeyPressed(rl.KeyP) {
+			fpsCounter = 0
+			start = false
+		}
+		if start {
+			fpsCounter++
+			if fpsCounter >= 30 {
+				fpsCounter = 0
+				update(&grid)
+
+			}
+		}
+		for y := range grid {
+			for x := range grid[y] {
+				if rl.IsMouseButtonDown(rl.MouseLeftButton) {
+					if rl.CheckCollisionPointRec(rl.GetMousePosition(), rl.Rectangle{float32(x * rl.GetScreenWidth() / len(grid[y])), float32(y * rl.GetScreenHeight() / len(grid)), float32(rl.GetScreenWidth() / len(grid[y])), float32(rl.GetScreenHeight() / len(grid))}) {
+						grid[y][x] = true
+					}
+				}
+			}
+		}
+
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.White)
+		for y := range grid {
+			for x := range grid[y] {
+				if grid[y][x] {
+					rl.DrawRectangle(int32(x*rl.GetScreenWidth()/len(grid[y])), int32(y*rl.GetScreenHeight()/len(grid)), int32(rl.GetScreenWidth()/len(grid[y])), int32(rl.GetScreenHeight()/len(grid)), rl.Green)
+				} else {
+					rl.DrawRectangleLines(int32(x*rl.GetScreenWidth()/len(grid[y])), int32(y*rl.GetScreenHeight()/len(grid)), int32(rl.GetScreenWidth()/len(grid[y])), int32(rl.GetScreenHeight()/len(grid)), rl.Black)
+				}
+			}
+		}
+		if start {
+			rl.DrawText("Started", 0, 0, 30, rl.Green)
+		} else {
+			rl.DrawText("stoped", 0, 0, 30, rl.Red)
+
+		}
+		rl.DrawFPS(200, 0)
+		rl.EndDrawing()
+	}
+	rl.CloseWindow()
+}
